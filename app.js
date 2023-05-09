e4319cf4d70d9f69c7656bee2a3afb8057a54d54
marcosdsantos <marcosdanielsantos47@gmail.com>
2023-05-09 19:11:43 -0300

    comitando2

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const app = express();
+
+// Configurações do servidor
+const PORT = 3000;
+
+// Conexão com o banco de dados
+mongoose.connect('mongodb://localhost/loja-online', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Conectado ao banco de dados');
+}).catch((err) => {
+  console.error('Erro ao conectar ao banco de dados:', err);
+});
+
+// Definição do modelo de produto
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  imageUrl: String
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+// Rotas do servidor
+app.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.render('index', { products });
+});
+
+// Configuração do view engine
+app.set('view engine', 'ejs');
+
+// Inicialização do servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
